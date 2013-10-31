d0d0e248376b817092730c87f905f9496728d2c5
Erik Larsen <erik_larsen@icloud.com>
2013-10-31 12:40:49 -0700

    Initial Commit

---
+++ sirfmon/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  sirfmon
+//
+//  Created by Erik Larsen on 10/31/13.
+//  Copyright (c) 2013 Erik Larsen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
